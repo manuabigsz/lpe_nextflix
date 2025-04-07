4913d1ef4d6aa0692145a22463503341d409eb3e
manuabigsz <manuelaossanes@hotmail.com>
2025-04-07 14:15:16 -0300

    feat: adding tipo video

--- src/bd/entities/Videos.js
+++ src/bd/entities/Videos.js
@@ -1,11 +1,12 @@
 class Video {
-    constructor(codigo, titulo, descricao, url_video,duracao,data_upload){
+    constructor(codigo, titulo, descricao, url_video,duracao,data_upload, tipo_video){
         this.codigo = codigo;
         this.titulo = titulo;
         this.descricao = descricao;
         this.url_video = url_video;
         this.duracao = duracao;
         this.data_upload = data_upload;
+        this.tipo_video = tipo_video;
     }
 }
 
